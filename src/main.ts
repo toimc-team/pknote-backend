9ffa96e416570264969bbb5e684a67f0b8767f68
brian <brian@toimc.com>
2021-07-27 01:18:16 +0800

    ci(项目): 添加husky,添加prettierignore&eslintignore,添加cz脚本与lint脚本,规范commit提交

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(123);
   await app.listen(3000);
 }
 bootstrap();
