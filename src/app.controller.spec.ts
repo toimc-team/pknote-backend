f8cc86c4549c7f4e9d0e718fe76545e2005c7dec
brian <brian@toimc.com>
2022-03-04 00:00:08 +0800

    feat(api): 集成swagger

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Nestjs! This is Pknote Backend Service!"', () => {
+      expect(appController.getHello()).toBe(
+        'Hello Nestjs! This is Pknote Backend Service!',
+      );
     });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('PkNote Swagger')
+    .setDescription('PkNote backend API')
+    .setVersion('1.0')
+    .addTag('beta')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
